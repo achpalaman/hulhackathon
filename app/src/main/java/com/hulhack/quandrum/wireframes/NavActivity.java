6dc39676a41eca9475db986e1ae21a72c9469f24
Aman Achpal <aman8achpal@gmail.com>
2016-03-13 19:05:41 +0530

    Got fragments working

--- app/src/main/java/com/hulhack/quandrum/wireframes/NavActivity.java
+++ app/src/main/java/com/hulhack/quandrum/wireframes/NavActivity.java
@@ -1,5 +1,7 @@
 package com.hulhack.quandrum.wireframes;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,6 +11,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.hulhack.quandrum.wireframes.fragments.AlertsFragment;
+import com.hulhack.quandrum.wireframes.fragments.ComplaintsFragment;
+import com.hulhack.quandrum.wireframes.fragments.FinanceFragment;
+import com.hulhack.quandrum.wireframes.fragments.SalesFragment;
+import com.hulhack.quandrum.wireframes.fragments.SupplyChainFragment;
 
 public class NavActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -19,6 +28,7 @@ public class NavActivity extends AppCompatActivity
         setContentView(R.layout.activity_nav);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setTitle("Main Menu");
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -65,23 +75,39 @@ public class NavActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+
+        FrameLayout frame = (FrameLayout) findViewById(R.id.content_frame);
+        frame.removeAllViews();
+        Fragment fragment = null;
+
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_alerts) {
-            // Handle the camera action
+            fragment = new AlertsFragment();
+            setTitle("Important Alerts");
+
         } else if (id == R.id.nav_complaints) {
+            fragment = new ComplaintsFragment();
+            setTitle("Complaints and Queries");
 
         } else if (id == R.id.nav_finance) {
+            fragment = new FinanceFragment();
+            setTitle("Financial Information");
 
         } else if (id == R.id.nav_sales) {
+            fragment = new SalesFragment();
+            setTitle("Sales and Marketing");
 
         } else if (id == R.id.nav_supply) {
+            fragment = new SupplyChainFragment();
+            setTitle("Supply Chain Management");
 
         } else if (id == R.id.nav_view) {
 
         }
-
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
